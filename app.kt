9d9155d0af41f0f6a50df7036f08783262d58755
BigShuff <stevedavis0504@gmail.com>
2023-01-20 23:04:59 +0000

    extended if with else pg19

--- app.kt
+++ app.kt
@@ -1,8 +1,10 @@
 fun main(){
-    val x = 3
-    val y = 1
+    val x = 1
+    val y = 3
     if (x > y){
         println("x is greater than y")
+    } else {
+        println("x is not greater than y")
     }
     println("This line runs no matter what")
 }
