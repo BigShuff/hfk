6877627bfb5523a69098ae8b98c07736a878a0a5
BigShuff <stevedavis0504@gmail.com>
2023-01-20 23:00:49 +0000

    conditional branching pg19

--- app.kt
+++ app.kt
@@ -1,9 +1,8 @@
 fun main(){
-    var x = 1
-    println("Before the loop x = $x.")
-    while (x < 4) {
-        println("In the loop. x = $x")
-        x = x + 1
+    val x = 3
+    val y = 1
+    if (x > y){
+        println("x is greater than y")
     }
-    println("After the loop x = $x.")
+    println("This line runs no matter what")
 }
