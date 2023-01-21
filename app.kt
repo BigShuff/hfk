a7280bc844a14fa479efe6a87681e1c851d31af4
BigShuff <stevedavis0504@gmail.com>
2023-01-21 15:11:58 +0000

    More concise way of if else

--- app.kt
+++ app.kt
@@ -1,10 +1,14 @@
 fun main(){
-    val x = 1
-    val y = 3
-    if (x > y){
+    val x = 3
+    val y = 1
+    /* if (x > y){
         println("x is greater than y")
     } else {
         println("x is not greater than y")
-    }
+    } */
+
+    // New more concise way of the same above
+
+    println(if( x > y) "x is greater than y" else "x is not greater than y")
     println("This line runs no matter what")
 }
